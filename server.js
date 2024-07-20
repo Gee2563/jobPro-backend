1b8f0dc24cadefd97b67541d765fa910c1cf532e
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-20 18:51:52 +0100

    create server + add user routes

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/UserRoutes')
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
