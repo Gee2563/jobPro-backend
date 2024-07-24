08d23e50c597918d16abafb2e1e412beb252c829
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-24 10:55:55 +0100

    feat: Add error handling to uploaded CV controller

--- controllers/uploadedCvController.js
+++ controllers/uploadedCvController.js
@@ -1,50 +1,69 @@
-const { get } = require('mongoose');
 const UploadedCV = require('../models/UploadedCV');
 
 // Get all uploaded CVs
 const getUploadedCVs = async (req, res) => {
-  const uploadedCVs = await UploadedCV.find({ user: req.user._id });
-  res.json(uploadedCVs);
+  try {
+    const uploadedCVs = await UploadedCV.find({ user: req.user._id });
+    res.json(uploadedCVs);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch uploaded CVs', error });
+  }
 };
 
 // If less than 3 CVs are uploaded, create a new CV
 const createUploadedCV = async (req, res) => {
-    if (getUploadedCVs.length < 3) {
-      const { cvComments, cvContent, cvFileName } = req.body;
+  try {
+    const userUploadedCVs = await UploadedCV.find({ user: req.user._id });
     
+    if (userUploadedCVs.length < 3) {
+      const { cvComments, cvContent, cvFileName } = req.body;
+
       const uploadedCV = new UploadedCV({
         user: req.user._id,
-        cvComments, cvContent, cvFileName,
+        cvComments,
+        cvContent,
+        cvFileName,
       });
-    
+
       const createdUploadedCV = await uploadedCV.save();
       res.status(201).json(createdUploadedCV);
     } else {
-        res.status(400).json({ message: 'You can only upload 3 CVs' });
-        }
+      res.status(400).json({ message: 'You can only upload 3 CVs' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to create uploaded CV', error });
+  }
 }
 
 // Get an uploaded CV by ID
 const getUploadedCVById = async (req, res) => {
-  const uploadedCV = await UploadedCV.findById(req.params.id);
+  try {
+    const uploadedCV = await UploadedCV.findById(req.params.id);
 
-  if (uploadedCV) {
-    res.json(uploadedCV);
-  } else {
-    res.status(404).json({ message: 'Uploaded CV not found' });
+    if (uploadedCV) {
+      res.json(uploadedCV);
+    } else {
+      res.status(404).json({ message: 'Uploaded CV not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch uploaded CV', error });
   }
 };
 
-// delete an uploaded CV
+// Delete an uploaded CV
 const deleteUploadedCV = async (req, res) => {
-  const uploadedCV = await UploadedCV.findById(req.params.id);
+  try {
+    const uploadedCV = await UploadedCV.findById(req.params.id);
 
-  if (uploadedCV) {
-    await uploadedCV.remove();
-    res.json({ message: 'Uploaded CV removed' });
-  } else {
-    res.status(404).json({ message: 'Uploaded CV not found' });
+    if (uploadedCV) {
+      await UploadedCV.deleteOne({ _id: req.params.id });
+      res.json({ message: 'Uploaded CV removed' });
+    } else {
+      res.status(404).json({ message: 'Uploaded CV not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete uploaded CV', error });
   }
-};
+}
 
 module.exports = { getUploadedCVs, createUploadedCV, getUploadedCVById, deleteUploadedCV };
