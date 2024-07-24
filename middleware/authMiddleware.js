f47ddd4d5f8e4dc37f2cb21ed009094af633cce6
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-24 17:23:13 +0100

    feat: Remove console.log statements from authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,11 +5,8 @@ const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-    console.log('Token:', token);
-
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log('Decoded:', decoded);
       req.user = await User.findById(decoded.id).select('-password');
       next();
     } catch (error) {
