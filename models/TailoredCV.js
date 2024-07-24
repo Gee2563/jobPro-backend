65269f2e9c0b9cbaae47bba777e39dea1ecc1c80
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-24 17:22:58 +0100

    feat: Add required fields to TailoredCV model

--- models/TailoredCV.js
+++ models/TailoredCV.js
@@ -1,12 +1,13 @@
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 
 const TailoredCvSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    originalCvContent: { type: String },
+    originalCvContent: { type: String, required: true },
     tailoredCvContent: { type: String },
+    companyWebsite: { type: String, required: true },
+    jobDescriptionUrl: { type: String, required: true },
     cvDateUploaded: { type: Date, default: Date.now }
-    });
+});
 
-
-const TailoredCv = mongoose.model('TailoredCV', TailoredCvSchema);
+const TailoredCv = mongoose.model('TailoredCv', TailoredCvSchema);
 module.exports = TailoredCv;
