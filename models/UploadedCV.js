0ef7cef41e8eda4c55f47cf0afad2f2b3503106d
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-24 17:22:50 +0100

    chore: Update UploadedCV model to include required fields

--- models/UploadedCV.js
+++ models/UploadedCV.js
@@ -1,13 +1,12 @@
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 
 const UploadedCVSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    cvComments: { type: String },
-    cvContent: { type: String },
-    cvDateUploaded: { type: Date, default: Date.now },
-    cvFileName: { type: String },
-    });
-
+    cvComments: { type: String, required: true },
+    cvContent: { type: String, required: true },
+    cvFileName: { type: String, required: true },
+    cvDateUploaded: { type: Date, default: Date.now }
+});
 
 const UploadedCV = mongoose.model('UploadedCV', UploadedCVSchema);
 module.exports = UploadedCV;
