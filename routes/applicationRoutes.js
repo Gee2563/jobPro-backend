f90d3ce9438d6c6a5cd61ad3a0c97497f06898d7
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-25 12:26:29 +0100

    chore: Update applicationRoutes.js to include updateApplicationStage endpoint

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const { getApplications, createApplication, updateApplication, deleteApplication, getApplicationById } = require('../controllers/applicationController');
+const { getApplications, createApplication, updateApplication, deleteApplication, getApplicationById , updateApplicationStage} = require('../controllers/applicationController');
 const authenticateToken = require('../middleware/authMiddleware'); // Import your middleware
 const router = express.Router();
 
 //Once authenticated, the user can access the protected routes
 //All applications
 router.route('/')
-  .get(authenticateToken, getApplications) // Use authenticateToken instead of protect
-  .post(authenticateToken, createApplication); // Use authenticateToken instead of protect
+  .get(authenticateToken, getApplications) 
+  .post(authenticateToken, createApplication); 
 
 //Single application
 router.route('/:id')
-  .get(authenticateToken, getApplicationById) // Use authenticateToken instead of protect
-  .put(authenticateToken, updateApplication) // Use authenticateToken instead of protect
-  .delete(authenticateToken, deleteApplication); // Use authenticateToken instead of protect
-
+  .get(authenticateToken, getApplicationById) 
+  .put(authenticateToken, updateApplication) 
+  .delete(authenticateToken, deleteApplication);
+  
+router.route('/stage/:id')
+  .put(authenticateToken, updateApplicationStage);
+  
 module.exports = router;
