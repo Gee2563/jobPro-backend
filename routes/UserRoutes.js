ce0af32ee22a7b65cb316f2e34010bbc8016448b
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-25 12:26:42 +0100

    refactor: Remove unnecessary console.log statements from UserRoutes

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -8,14 +8,17 @@ router.post('/register', registerUser);
 
 // Route to log in a user
 router.post('/login', (req, res, next) => {
-  console.log('Login route hit');
-  console.log('Authorization header:', req.headers.authorization);
   next();
 }, loginUser);
 
 // Route to get user info
 router.get('/me', authenticateToken, (req, res) => {
   res.json(req.user);
+});
+
+// Route to update user password
+router.put('/password', authenticateToken, (req, res) => {
+  res.send('Update password');
 });
 
 module.exports = router;
