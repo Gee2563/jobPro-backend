b90a645d65667194bde7f81cfe17402973143d97
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-25 12:26:23 +0100

    feat: Add logging statements for debugging in authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,18 +3,21 @@ const User = require('../models/User'); // Adjust the path as necessary
 
 const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
-      next();
-    } catch (error) {
-      return res.sendStatus(403); // Forbidden
-    }
-  } else {
-    res.sendStatus(401); // Unauthorized
+  console.log('Auth header:', authHeader);
+if (authHeader) {
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('Decoded token:', decoded); // Log decoded token to verify user info
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (error) {
+    return res.sendStatus(403); // Forbidden
   }
+} else {
+  res.sendStatus(401); // Unauthorized
+}
+
 };
 
 module.exports = authenticateToken;
